9514af075e4bc6b6fa3fe808138da0bdcb8bbf3c
TanakaMafuta <145983339+TanakaMafuta@users.noreply.github.com>
2025-07-21 03:48:08 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "CyberGhost Bot",
   botOwner: 'CYBER-GHOST', // Your name
   ownerNumber: '263785061900', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
